29bbc991ba1f9274dfe39ee9ae3715ce9cbcae77
anuj taywade <anujtaywade456@gmail.com>
2026-01-28 21:33:58 +0530

    DB setup

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,1 +1,14 @@
-const 
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+        const conn = mongoose.connect(process.env.MONGO_URL)
+        
+        console.log(`Connected to DB ${(await conn).connection.host}.`)
+    } catch (error) {
+        console.log(`Error : ${error.message}`)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB ;

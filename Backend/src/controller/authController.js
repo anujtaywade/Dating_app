ed39f6a21ce02f840a199b9de590d9518d70447a
anuj taywade <anujtaywade456@gmail.com>
2026-01-31 23:01:09 +0530

    login controller

--- Backend/src/controller/authController.js
+++ Backend/src/controller/authController.js
@@ -1,5 +1,24 @@
-
+const brcypt = require("bcrypt")
 
 exports.login=async (req,es) => {
-    
+    try {
+        const {email , password} = req.body ;
+
+        if (!email , !password , !name) {
+            return res.send(400).json({message : "All feilds are mandatory"})
+        }
+        
+        const existingUser = await user.findOne({email})
+
+        if (existingUser) {
+            res.send(409).json({message : "user already exist"})
+        }
+
+        const hashedPassword = await brcypt.hash(password,10)
+
+        const
+
+    } catch (error) {
+        
+    }
 }

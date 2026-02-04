90dc69f9c859244855bff325cd75a48ea8f046c2
anuj taywade <anujtaywade456@gmail.com>
2026-02-04 21:21:44 +0530

    jwt signin

--- Backend/src/controller/authController.js
+++ Backend/src/controller/authController.js
@@ -1,49 +1,55 @@
+const jwt = require("jsonwebtoken");
 
-exports.login=async (req,res) => {
-    try {
-        const {phone , googleId} = req.body ;
+exports.login = async (req, res) => {
+  try {
+    const { phone, googleId } = req.body;
 
-        if (!phone && !googleId ) {
-            return res.status(400).json({message : "Phone or google login required"})
-        }
-        
-        let authUser ;
-
-        if (phone) {
-            authUser = await user.findOne({phone})
-
-            if(!user){
-                authUser =await user.create({
-                    phone,
-                    authProvider : "phone",
-                    isVerified : true
-                })
-            }
-        }
-
-        if (googleId){
-            authUser = await user.findOne({googleId})
-
-            if (!googleId){
-                authUser =await user.create({
-                    googleId,
-                    authProvider : "google",
-                    isVerified : true
-                })
-            }
-        }
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({message : "Internal Server error"})
+    if (!phone && !googleId) {
+      return res
+        .status(400)
+        .json({ message: "Phone or google login required" });
     }
-}
 
-exports.logout = async (req,res) => {
-    try {
-         res.status(200).json({message : "Logout successful"})
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({message : "Internl Server Error"})
+    let authUser;
+
+    if (phone) {
+      authUser = await user.findOne({ phone });
+
+      if (!authUser) {
+        authUser = await user.create({
+          phone,
+          authProvider: "phone",
+          isVerified: true,
+        });
+      }
     }
-}
+
+    if (googleId) {
+      authUser = await user.findOne({ googleId });
+
+      if (!authUser) {
+        authUser = await user.create({
+          googleId,
+          authProvider: "google",
+          isVerified: true,
+        });
+      }
+    }
+    const token = jwt.sign({ id: authUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).json({ message: "Login Sucessfull" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server error" });
+  }
+};
+
+exports.logout = async (req, res) => {
+  try {
+    res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internl Server Error" });
+  }
+};

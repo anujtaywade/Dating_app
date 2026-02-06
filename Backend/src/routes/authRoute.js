1daa99308834ae34d4cbea26aae88a31d37f9459
anuj taywade <anujtaywade456@gmail.com>
2026-02-06 21:25:21 +0530

    auth route protected

--- Backend/src/routes/authRoute.js
+++ Backend/src/routes/authRoute.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 const {login,logout} = require('../controller/authController')
+const {authMe} = require('../controller/authController')
+const {protect} = require('../middleware/authMiddleware')
+
 
 router.post('/login',login)
 router.post('/logout',logout)
+router.get('/me',authMe,protect)
 
 module.exports = router

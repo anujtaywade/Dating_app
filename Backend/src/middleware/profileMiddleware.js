bd2fc7d90a354e0190f930286571dd403e8e3775
anuj taywade <anujtaywade456@gmail.com>
2026-04-13 19:20:19 +0530

    profile middleware added

---
+++ Backend/src/middleware/profileMiddleware.js
@@ -0,0 +1,29 @@
+const User = require("../models/User");
+
+exports.profileCompleteCheck = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    if (!user.profileCompleted) {
+      return res.status(403).json({
+        message: "Complete your profile to use this feature"
+      });
+    }
+
+    next();
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+};

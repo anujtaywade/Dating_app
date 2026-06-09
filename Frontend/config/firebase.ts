c711a440aec3e6c3e7fdb2e5d96ca561d531be7c
anuj taywade <anujtaywade456@gmail.com>
2026-06-10 03:28:43 +0530

    typescript error fix

--- Frontend/config/firebase.ts
+++ Frontend/config/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { initializeAuth, getAuth } from 'firebase/auth';
+import { initializeAuth, getAuth, Auth } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
@@ -13,7 +13,7 @@ const firebaseConfig = {
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-let auth;
+let auth: Auth;
 try {
   // @ts-ignore
   const { getReactNativePersistence } = require('firebase/auth');

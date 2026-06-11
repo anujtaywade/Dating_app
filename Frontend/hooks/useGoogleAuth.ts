01a38d7008503974ee6f74db9cc937a277184ed3
anuj taywade <anujtaywade456@gmail.com>
2026-06-11 16:49:39 +0530

    google login debugging session 2

--- Frontend/hooks/useGoogleAuth.ts
+++ Frontend/hooks/useGoogleAuth.ts
@@ -7,8 +7,6 @@ export function useGoogleLogin() {
   return Google.useAuthRequest({
     androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
     webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
-    redirectUri: "https://auth.expo.io/@anujtaywade/Frontend",
     scopes: ["profile", "email"],
-    usePKCE: false,
   });
 }

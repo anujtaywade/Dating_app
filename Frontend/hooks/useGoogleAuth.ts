2243aa0077f1a14a5664de3e44325bbfe509217f
anuj taywade <anujtaywade456@gmail.com>
2026-06-16 22:44:56 +0530

    Google auth debugging session 4

--- Frontend/hooks/useGoogleAuth.ts
+++ Frontend/hooks/useGoogleAuth.ts
@@ -5,13 +5,16 @@ WebBrowser.maybeCompleteAuthSession();
 
 export function useGoogleLogin() {
   const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
-    webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
-    scopes: ["profile", "email"],
-    usePKCE: false,
-  });
+  clientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+  androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
+  webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+  scopes: ["profile", "email"],
+  usePKCE: true,
+});
 
   console.log("REQUEST IS READY:", !!request);
+  console.log("ANDROID ID:", process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID);
+console.log("WEB ID:", process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID);
 
   return [request, response, promptAsync] as const;
 }

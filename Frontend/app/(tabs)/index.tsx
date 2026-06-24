9b00022c835c1cb3e7404713912d67cde5d50639
anuj taywade <anujtaywade456@gmail.com>
2026-06-24 19:55:18 +0530

    fetch location working properly and redirecting to home page

--- Frontend/app/(tabs)/index.tsx
+++ Frontend/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login success by google</Text>
+      <Text style={styles.title}>Home page </Text>
     </View>
   );
 }
